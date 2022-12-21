fde8362af13d34cad4947bc3609876087b11b5fa
bruno.ramos <brboddhd2@gmail.com>
2022-12-21 09:28:04 -0300

    fix

---
+++ src/documin/documento/elementos/Lista.java
@@ -0,0 +1,18 @@
+package documin.documento.elementos;
+
+public class Lista extends ElementoAbstract{
+
+    public Lista(String valor, String propiedades) {
+        super(valor, propiedades);
+    }
+
+    @Override
+    public String representacaoCompleta() {
+        return null;
+    }
+
+    @Override
+    public String representacaoResumida() {
+        return null;
+    }
+}
